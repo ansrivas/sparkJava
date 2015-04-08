7fe0e8c0c3900852724a154c09f1a14a2ec5373e
Ankur <best.ankur@gmail.com>
2015-04-08 11:35:09 +0200

    a very minimal java program to prepare the eclipse environment to use with apache spark

---
+++ NumberGenerator/src/main/java/com/mkinput/App.java
@@ -0,0 +1,29 @@
+package com.mkinput;
+
+
+/**
+ * Hello world!
+ *
+ */
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.Function;
+
+public class App {
+  public static void main(String[] args) {
+    String logFile = "/home/user/spark/spark/README.md"; // Should be some file on your system
+    JavaSparkContext sc = new JavaSparkContext("local", "Simple App",
+      "/home/user/spark/spark", new String[]{"target/NumberGenerator-1.0-SNAPSHOT.jar"});
+    JavaRDD<String> logData = sc.textFile(logFile).cache();
+
+    long numAs = logData.filter(new Function<String, Boolean>() {
+      public Boolean call(String s) { return s.contains("a"); }
+    }).count();
+
+    long numBs = logData.filter(new Function<String, Boolean>() {
+      public Boolean call(String s) { return s.contains("b"); }
+    }).count();
+
+    System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+  }
+}
+
